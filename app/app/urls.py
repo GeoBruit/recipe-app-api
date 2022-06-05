17dd333cf1fe166ef49c2919e8399dc492383583
GeoBruit <george_cioboata@yahoo.com>
2022-06-05 23:02:17 +0100

    Finished, Getting ready for deployment

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from re import I
+from xml.dom.minidom import Document
 from xml.etree.ElementInclude import include
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -20,6 +22,8 @@ from drf_spectacular.views import (
 )
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -33,3 +37,9 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
